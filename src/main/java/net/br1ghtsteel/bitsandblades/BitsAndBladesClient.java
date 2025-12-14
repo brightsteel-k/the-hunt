8a0b1a45edc75bc8b843507ac437ccf8b3276d2a
brightsteel-k <kmbrent@email.com>
2025-12-14 14:14:05 -0800

    Add initial fabric mod starter files

---
+++ src/main/java/net/br1ghtsteel/bitsandblades/BitsAndBladesClient.java
@@ -0,0 +1,10 @@
+package net.br1ghtsteel.bitsandblades;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BitsAndBladesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/br1ghtsteel/bitsandblades/BitsAndBladesDataGenerator.java
@@ -0,0 +1,11 @@
+package net.br1ghtsteel.bitsandblades;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BitsAndBladesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
